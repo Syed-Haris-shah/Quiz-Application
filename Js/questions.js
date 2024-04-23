c703c8448be91e4dcb1b96c6c40cbf647293f9d4
Syed Haris Shah <haris.official222@gmail.com>
2024-04-23 13:24:36 +0500

    push quiz app

---
+++ Js/questions.js
@@ -0,0 +1,120 @@
+// Quiz Questions
+
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+  {
+    numb: 6,
+    question: "Which type of JavaScript language is.?",
+    answer: "Object-Based",
+    options: [
+        "Object-Oriendted",
+        "Object-Based",
+        "Assembly-Language",
+        "High-Level"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: " Which of the following methods can be used to display data in some form using Javascript?",
+    answer: "All of the above",
+    options: [
+        "document.write()",
+        "console.log()",
+        "window.alert()",
+        "All of the above"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "How can a datatype be declared to be a constant type?",
+    answer: "const",
+    options: [
+        "const",
+        "var",
+        "let",
+        "constant"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+    answer: "Object",
+    options: [
+        "Boolean",
+        "Object",
+        "Undefined",
+        "integer"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answer: "Both A and B",
+    options: [
+        "var",
+        "let",
+        "Both A and B",
+        "None of the above"
+    ]
+  }
+];
